ca885618d9c0b7af77263cdfc1c162e8b258cebf
Omar Zahid <57479337+omar-zahid@users.noreply.github.com>
2022-11-03 10:50:43 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="section">
+      <div className="title">Welcome to the section</div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+        nisi ducimus vero suscipit fuga porro repellendus similique voluptate
+        odio eos distinctio officiis in iusto sapiente velit quos placeat, vel
+        doloribus commodi! Velit.
+      </p>
+    </div>
+  );
+}
